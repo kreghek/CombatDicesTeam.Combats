a274de5ca9bbc520cda0b222cb1bc5f0c50d2ef0
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-10-19 04:13:33 +0700

    Fix tests

--- src/CombatDicesTeam.Combats/CombatEngineBase.cs
+++ src/CombatDicesTeam.Combats/CombatEngineBase.cs
@@ -543,5 +543,7 @@ public abstract class CombatEngineBase
     [PublicAPI]
     public event EventHandler<CombatantEffectEventArgs>? CombatantEffectHasBeenDispeled;
 
-    public record StatDamage(int Amount, int SourceAmount);
+    public sealed record StatDamage(int Amount, int SourceAmount) {
+        public static implicit operator StatDamage(int monoValue) { return new StatDamage(monoValue, monoValue); }
+    };
 }
