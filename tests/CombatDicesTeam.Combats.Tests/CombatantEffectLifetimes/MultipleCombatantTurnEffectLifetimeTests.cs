43356f4845fd047598fccfd5be5b24d89cbba620
Pavel Kurkutov <kurkutovpa@gmail.com>
2024-03-12 21:47:27 +0700

    Fix error in multi turn lifetime

---
+++ tests/CombatDicesTeam.Combats.Tests/CombatantEffectLifetimes/MultipleCombatantTurnEffectLifetimeTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using CombatDicesTeam.Combats.CombatantEffectLifetimes;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombatDicesTeam.Combats.CombatantEffectLifetimes.Tests;
+
+[TestFixture()]
+public class MultipleCombatantTurnEffectLifetimeTests
+{
+    [Test()]
+    public void UpdateTest()
+    {
+        // ARRANGE
+
+        var sut = new MultipleCombatantTurnEffectLifetime(1);
+
+        // ACT
+
+        sut.Update(CombatantStatusUpdateType.EndCombatantTurn, Mock.Of<ICombatantStatusLifetimeUpdateContext>());
+
+        // ASSERT
+
+        sut.IsExpired.Should().BeFalse();
+    }
+
+    [Test()]
+    public void UpdateTest2()
+    {
+        // ARRANGE
+
+        var sut = new MultipleCombatantTurnEffectLifetime(1);
+
+        // ACT
+
+        sut.Update(CombatantStatusUpdateType.EndCombatantTurn, Mock.Of<ICombatantStatusLifetimeUpdateContext>());
+        sut.Update(CombatantStatusUpdateType.EndRound, Mock.Of<ICombatantStatusLifetimeUpdateContext>());
+        sut.Update(CombatantStatusUpdateType.EndCombatantTurn, Mock.Of<ICombatantStatusLifetimeUpdateContext>());
+
+        // ASSERT
+
+        sut.IsExpired.Should().BeTrue();
+    }
+
+    [Test()]
+    public void UpdateTest3()
+    {
+        // ARRANGE
+
+        var sut = new MultipleCombatantTurnEffectLifetime(1);
+
+        // ACT
+
+        sut.Update(CombatantStatusUpdateType.EndCombatantTurn, Mock.Of<ICombatantStatusLifetimeUpdateContext>());
+        sut.Update(CombatantStatusUpdateType.EndRound, Mock.Of<ICombatantStatusLifetimeUpdateContext>());
+
+        // ASSERT
+
+        sut.IsExpired.Should().BeFalse();
+    }
+}
